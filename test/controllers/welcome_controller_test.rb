901de3a8bd5573f4a049b1457d28bc1592ba6bf9
GitLab <root@localhost>
2019-08-07 22:51:51 +0200

    Initialized from 'Ruby on Rails' project template

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+    assert_select "body", "You're on Rails!"
+  end
+
+end
